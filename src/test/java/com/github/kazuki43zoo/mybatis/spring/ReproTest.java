65efc28102a16b6e7176ae30ad2316293683f54c
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-03-20 23:21:27 +0900

    Workaround for 3.3.x

--- src/test/java/com/github/kazuki43zoo/mybatis/spring/ReproTest.java
+++ src/test/java/com/github/kazuki43zoo/mybatis/spring/ReproTest.java
@@ -1,14 +1,22 @@
 package com.github.kazuki43zoo.mybatis.spring;
 
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
-import org.mybatis.spring.MyBatisSystemException;
+import org.mybatis.spring.MyBatisExceptionTranslator;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.ConnectionHolder;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -16,9 +24,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
@@ -48,7 +60,7 @@ public class ReproTest {
         }
     }
 
-    @Test (expected = TransactionException.class)
+    @Test(expected = TransactionException.class)
     public void jdbcTemplateAfterTimeout() throws InterruptedException {
         try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(JdbcTemplateDataSourceContext.class)) {
             TransactionalService service = ctx.getBean(TransactionalService.class);
@@ -76,8 +88,22 @@ public class ReproTest {
         SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
             SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
             factoryBean.setDataSource(dataSource);
+            factoryBean.setPlugins(new Interceptor[]{transactionTimeoutReflector()});
             return factoryBean;
         }
+
+        @Bean
+        SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory factory) {
+            return new SqlSessionTemplate(factory,
+                    factory.getConfiguration().getDefaultExecutorType(),
+                    new TransactionalMyBatisExceptionTranslator(dataSource()));
+        }
+
+        @Bean
+        TransactionTimeoutReflector transactionTimeoutReflector() {
+            return new TransactionTimeoutReflector();
+        }
+
 
         @Bean
         TransactionalService service(Mapper mapper) {
@@ -134,5 +160,61 @@ public class ReproTest {
         @Select("SELECT CURRENT_TIMESTAMP()")
         Timestamp findCurrentTimestamp();
     }
+
+    @Intercepts({@Signature(type = StatementHandler.class, method = "prepare", args = Connection.class)})
+    static class TransactionTimeoutReflector implements Interceptor {
+
+        @Autowired
+        DataSource dataSource;
+
+        @Override
+        public Object intercept(Invocation invocation) throws Throwable {
+            Statement statement = (Statement) invocation.proceed();
+            ConnectionHolder holder =
+                    (ConnectionHolder) TransactionSynchronizationManager.getResource(dataSource);
+            if (holder != null && holder.hasTimeout()) {
+                // ↓ 既にタイムアウトになっている場合は、このタイミングでTransactionTimedOutExceptionが発生する。
+                int transactionTimeToLiveSec = holder.getTimeToLiveInSeconds();
+                int currentQueryTimeoutSec = statement.getQueryTimeout();
+                // ↓ クエリタイムアウトにトランザクションタイムアウトを反映
+                if (currentQueryTimeoutSec == 0 || transactionTimeToLiveSec < currentQueryTimeoutSec) {
+                    statement.setQueryTimeout(transactionTimeToLiveSec);
+                }
+            }
+            return statement;
+        }
+
+        @Override
+        public Object plugin(Object target) {
+            return Plugin.wrap(target, this);
+        }
+
+        @Override
+        public void setProperties(Properties properties) {
+        }
+    }
+
+    static class TransactionalMyBatisExceptionTranslator extends MyBatisExceptionTranslator {
+
+        public TransactionalMyBatisExceptionTranslator(DataSource dataSource) {
+            super(dataSource, true);
+        }
+
+        @Override
+        public DataAccessException translateExceptionIfPossible(RuntimeException e) {
+            if (e instanceof PersistenceException) {
+                if (e.getCause() instanceof PersistenceException) {
+                    e = (PersistenceException) e.getCause();
+                }
+                // ↓ SpringのTransactionExceptionはそのままスロー
+                if (e.getCause() instanceof TransactionException) {
+                    throw (TransactionException) e.getCause();
+                }
+            }
+            return super.translateExceptionIfPossible(e);
+        }
+
+    }
+
 
 }
